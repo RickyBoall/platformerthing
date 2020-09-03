e4807f49a0bda3d7bb229c2fbcbd13e2e36a2d81
NoodleMonstie <47699677+NoodleMonstie@users.noreply.github.com>
2020-09-02 23:05:54 -0400

    Update Display.js

--- main/Display.js
+++ main/Display.js
@@ -1,3 +1,14 @@
-//Bridge between game file and HTML, this runs once
+//Bridge between game file and HTML
 
-//
+//runs once
+
+//grab terrain from spreadsheet
+
+//populate HTML with proper terrain (terrain setup)
+
+//insert sprites at starting locations
+
+
+
+
+//grab new sprite locations, and update (called from game loop, runs every time game has another loop)
