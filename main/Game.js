0ea195a72a072187c13309754e5242f7102cb474
NoodleMonstie <47699677+NoodleMonstie@users.noreply.github.com>
2020-09-02 22:56:19 -0400

    Update Game.js

--- main/Game.js
+++ main/Game.js
@@ -1,22 +1,22 @@
-#Controls game logic (already in game loop)
-#Includes: enemy movement, who's turn it is to move, gravity/ physics etc.
+//Controls game logic (already in game loop)
+//includes: enemy movement, who's turn it is to move, gravity/ physics etc.
 
-#1 Check for collision:
-#         if player collides with enemy -> update health bars
-#         if player collides with platforms update speed and location etc.
-#         if player collides with platforms update speed and location etc.
+//1 Check for collision:
+//if player collides with enemy -> update health bars
+//        if player collides with platforms update speed and location etc.
+//        if player collides with platforms update speed and location etc.
 
-#2 check enemy health
-#        if at 0 -> remove or death animation
+//2 check enemy health
+//        if at 0 -> remove or death animation
 
-#3 check hero health
-#        if at 0 -> end of game / death animation or - life and continue
-
-
-#4 check for player movement -> move character
+//3 check hero health
+//        if at 0 -> end of game / death animation or - life and continue
 
 
-#5 update enemy placement (move enemy sprites)
+//4 check for player movement -> move character
 
-#6 win check 
-# TBD(for now check if reached flag)
+
+//5 update enemy placement (move enemy sprites)
+
+// win check 
+// TBD(for now check if reached flag)
