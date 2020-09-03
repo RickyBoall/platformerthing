bd1515fea458ab8399ba9405014155cb53af7b54
NoodleMonstie <47699677+NoodleMonstie@users.noreply.github.com>
2020-09-02 23:11:59 -0400

    Update Controller.js

--- main/Controller.js
+++ main/Controller.js
@@ -1,1 +1,19 @@
-#Controller Class -> covers Player movement (aka user inputs)
+//Controller Class -> covers Player movement (aka user inputs)
+
+//initialize game
+  //create player obj
+  //create enemy objs
+  //create terrain objs
+  //trigger display and game logic
+
+//check for user input
+
+  //w or spacebar -> jump
+
+  //d  ->  move right
+
+  //a  ->  move left
+
+  //s  ->  move through (down) some platforms (thin ones), can use solid state
+
+  //right-click or left-click  ->  attack
