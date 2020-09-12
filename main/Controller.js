5af530c3db4bf93d93edb1f2b187832d1e91c297
Sukhraj Ricky Singh Boall <60629268+RickyBoall@users.noreply.github.com>
2020-09-11 23:04:02 -0400

    Update Controller.js

--- main/Controller.js
+++ main/Controller.js
@@ -1,32 +1,34 @@
-//Controller Class -> covers Player movement (aka user inputs)
-//Initiaze the canvas
-function renderCanvas(){
-  ctx.fillStyle = "#000000";
-  ctx.fillRect(0, 0, width, width);
+class Controller(){
+  //Controller Class -> covers Player movement (aka user inputs)
+  //Initiaze the canvas
+  function renderCanvas(){
+    ctx.fillStyle = "#000000";
+    ctx.fillRect(0, 0, width, width);
+  }
+
+  this.canvas = document.getElementById('canvas');
+  this.ctx = canvas.getContext("2d");
+  this.width = 1200;
+  this.height = 600;
+  ctx.canvas.height = height;
+  ctx.canvas.width = width;
+  renderCanvas();
+
+  //initialize game
+    //create player obj
+    //create enemy objs
+    //create terrain objs
+    //trigger display and game logic
+
+  //check for user input
+
+    //w or spacebar -> jump
+
+    //d  ->  move right
+
+    //a  ->  move left
+
+    //s  ->  move through (down) some platforms (thin ones), can use solid state
+
+    //right-click or left-click  ->  attack
 }
-
-var canvas = document.getElementById('canvas');
-var ctx = canvas.getContext("2d");
-var width = 1200;
-var height = 600;
-ctx.canvas.height = height;
-ctx.canvas.width = width;
-renderCanvas();
-
-//initialize game
-  //create player obj
-  //create enemy objs
-  //create terrain objs
-  //trigger display and game logic
-
-//check for user input
-
-  //w or spacebar -> jump
-
-  //d  ->  move right
-
-  //a  ->  move left
-
-  //s  ->  move through (down) some platforms (thin ones), can use solid state
-
-  //right-click or left-click  ->  attack
