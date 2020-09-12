03218a0d785f1150fc4598a593ddcee5610869b3
NoodleMonstie <47699677+NoodleMonstie@users.noreply.github.com>
2020-09-11 23:01:30 -0400

    Update Enemy.js

--- main/Enemy.js
+++ main/Enemy.js
@@ -1,6 +1,6 @@
 //this is the Enemy class
 import Entity from 'Entity.js'
-class Enemy() extends Entity{
+export class Enemy() extends Entity{
   constructor(HP, xloc, yloc, AP, defence, speed, jumpHeight){
     super(HP, xloc, yloc, AP, defence, speed, jumpHeight);
   }
