d33d808d1d8602155bb2e3c9fdeaa760ada72066
Sukhraj Ricky Singh Boall <60629268+RickyBoall@users.noreply.github.com>
2020-09-11 22:42:44 -0400

    Added Canvas to controller

--- main/Controller.js
+++ main/Controller.js
@@ -1,4 +1,17 @@
 //Controller Class -> covers Player movement (aka user inputs)
+//Initiaze the canvas
+function renderCanvas(){
+  ctx.fillStyle = "#000000";
+  ctx.fillRect(0, 0, width, width);
+}
+
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext("2d");
+var width = 1200;
+var height = 600;
+ctx.canvas.height = height;
+ctx.canvas.width = width;
+renderCanvas();
 
 //initialize game
   //create player obj
