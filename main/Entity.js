458c59129262f0bb5187a07480e0ff4a7d5bce3c
NoodleMonstie <47699677+NoodleMonstie@users.noreply.github.com>
2020-09-11 22:18:04 -0400

    Update Entity.js

--- main/Entity.js
+++ main/Entity.js
@@ -1,3 +1,38 @@
 //this is the superclass Entity --> is a virtual class
 
-/********************************************************
+/********************************************************/
+
+class Entity{
+  constructor(HP, xloc, yloc, AP, defence, speed, jumpHeight){
+    this.HP = HP;
+    this.x = xloc;
+    this.y = yloc;
+    this.AP = AP;
+    this.defence = defence;
+    this.speed = speed;
+    this.jumpHeight = jumpHeight;
+  }
+  
+  move(){
+    this.x = this.x + this.speed;
+  }
+  
+  //matt helppp pysicssss
+  jump(){
+  }
+  
+  attack(opponent){
+    opponent.HP = opponent.HP - (this.attack - (opponent.defense/2));
+  }
+  
+  checkHP(){
+    if(this.HP =< 0){
+      this.die();
+    }
+  }
+  
+  die(){
+    //trigger death animation
+    
+  }
+}
